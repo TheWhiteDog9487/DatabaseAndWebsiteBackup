9020b986ae7f59f61d3947f577ca22dab1d9b7c4
TheWhiteDog9487 <thewhitedog9487@outlook.com>
2025-11-26 17:15:43 +0800

    解决多进程导致的探测Zstd算法日志重复打印

--- Backup.py
+++ Backup.py
@@ -14,13 +14,7 @@ from ProcessTimer import MeasureExecutionTime
 
 ZipWorker: Optional[ProcessPoolExecutor] = None
 DontCompressFileExtensions = (".mp4", ".mkv", ".zip", "tar.gz")
-
-try:
-    logging.info("您的Python版本支持ZStandard，将使用Zstandard算法进行压缩。")
-    Algorithms = zipfile.ZIP_ZSTANDARD
-except AttributeError:
-    logging.warning("当前Python版本不支持Zstandard算法，将使用Deflate算法进行压缩。")
-    Algorithms = zipfile.ZIP_DEFLATED
+Algorithms: int = 0
 
 def ZipDirectoryTree(ZipFileName: str, TargetDirectory: Path):
     with zipfile.ZipFile(ZipFileName, "w", Algorithms) as ZipFile:
